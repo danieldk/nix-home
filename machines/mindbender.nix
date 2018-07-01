c91ddd359b30a514e66a27ec997ae93726b4c172
Daniël de Kok <me@danieldk.eu>
2018-07-01 20:23:45 +0200

    mindbender: fix import paths.

--- machines/mindbender.nix
+++ machines/mindbender.nix
@@ -4,13 +4,13 @@ let
   unstable = import <nixos-unstable> {};
 in {
   imports = [
-    ./cfg/dropbox.nix
-    ./cfg/git.nix
-    ./cfg/fzf.nix
-    ./cfg/rust.nix
-    ./cfg/ssh.nix
-    ./cfg/vim.nix
-    ./cfg/zsh.nix
+    ../cfg/dropbox.nix
+    ../cfg/git.nix
+    ../cfg/fzf.nix
+    ../cfg/rust.nix
+    ../cfg/ssh.nix
+    ../cfg/vim.nix
+    ../cfg/zsh.nix
   ];
 
   home.packages = with pkgs; [
