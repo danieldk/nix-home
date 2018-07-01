f1d365943cfa2fa13d7a60b537b4755b534985ee
Daniël de Kok <me@danieldk.eu>
2018-07-01 19:34:01 +0200

    Add direnv.

--- cfg/zsh.nix
+++ cfg/zsh.nix
@@ -1,8 +1,14 @@
 { pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    direnv
+  ];
+
   programs.zsh = {
     enable = true;
+
+    initExtra = builtins.readFile zsh/zshrc;
 
     oh-my-zsh = {
       enable = true;
