084852832c22a43ac82f090355afcc9ba421d649
Daniël de Kok <me@danieldk.eu>
2018-07-04 20:21:10 +0200

    jupyterEnv: work on NixOS and Darwin.
    (Needs to be refined.)

--- overlays/20-python-envs.nix
+++ overlays/20-python-envs.nix
@@ -1,7 +1,7 @@
 self: super: {
 
 jupyterEnv = with super; let
-  unstable = import <nixpkgs-unstable> {};
+  unstable = if stdenv.isDarwin then import <nixpkgs-unstable> {} else import <nixos-unstable> {};
   skl = unstable.python36.pkgs.scikitlearn.overridePythonAttrs (oldAttrs: { checkPhase = ""; });
 in unstable.python36.withPackages (ps: with ps; [
   matplotlib
