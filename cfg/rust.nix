428a080d32e8485f7f41295a948a3c12ffcb431f
Daniël de Kok <me@danieldk.eu>
2018-07-05 17:08:04 +0200

    Install racer, set RUST_SRC_PATH.

--- cfg/rust.nix
+++ cfg/rust.nix
@@ -3,5 +3,8 @@
 {
   home.packages = with pkgs; [
     latest.rustChannels.stable.rust
+    rustracer
   ];
+
+  programs.zsh.initExtra = "export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src";
 }
