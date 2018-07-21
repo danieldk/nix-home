5dad55f17e953e293419ff2fafadb26102440054
Daniël de Kok <me@danieldk.eu>
2018-07-21 12:51:45 +0200

    Add B2 backup script.

--- cfg/restic.nix
+++ cfg/restic.nix
@@ -10,6 +10,7 @@ in {
 
   home.file = {
     ".config/restic/env".source = ./restic/env;
+    "bin/backup-b2".source = ./restic/backup-b2;
     "bin/backup-local".source = ./restic/backup-local;
   };
 }
