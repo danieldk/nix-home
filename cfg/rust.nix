517328ce9f5ef07101d53867d13bf19598ca9dd5
Daniël de Kok <me@danieldk.eu>
2018-07-25 08:20:59 +0200

    Use racer from master.

--- cfg/rust.nix
+++ cfg/rust.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [
     latest.rustChannels.stable.rust
-    (rustracer.overrideAttrs (attrs: { doCheck = false; }))
+    danieldk.rustracer
   ];
 
   programs.zsh.initExtra = "export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src";
