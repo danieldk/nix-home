56e8f8ea7781a01efe848d3c489fa22c6a9af3df
Daniël de Kok <me@danieldk.eu>
2018-07-28 16:28:40 +0200

    alpinocorpus: 2.8.2 -> 2.8.3

--- overlays/30-alpinocorpus.nix
+++ overlays/30-alpinocorpus.nix
@@ -1,20 +1,18 @@
 self: super: {
 
-alpinocorpus = with super; let
-  unstable = if stdenv.isDarwin then import <nixpkgs-unstable> {} else import <nixos-unstable> {};
-in stdenv.mkDerivation rec {
+alpinocorpus = with super; stdenv.mkDerivation rec {
   name = "alpinocorpus-${version}";
-  version = "2.8.2";
+  version = "2.8.3";
 
   src = fetchFromGitHub {
     owner = "rug-compling";
     repo = "alpinocorpus";
-    rev = "2.8.2";
-    sha256 = "17lfa3104653j0v1kis7jzp70lpdy8l89f6wwlh3mbjw2bi8hj7z";
+    rev = "${version}";
+    sha256 = "0xrviphcawxgwfljkvvh162xqca215j53w9xyjgralr2pzslr5cc";
   };
 
   nativeBuildInputs = [
-    unstable.cmake
+    cmake pkgconfig
   ];
 
   buildInputs = [
