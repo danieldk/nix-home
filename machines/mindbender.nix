d1d4f82f661c3d7639fcd44b040c99dcba4b8551
Daniël de Kok <me@danieldk.eu>
2018-08-01 20:30:03 +0200

    GDK scaling.

--- machines/mindbender.nix
+++ machines/mindbender.nix
@@ -50,5 +50,7 @@
 
   home.sessionVariables = {
     NIX_PATH = "$HOME/git/nixpkgs:nixpkgs=$HOME/git/nixpkgs:nixpkgs-unstable=$HOME/git/nixpkgs";
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
   };
 }
