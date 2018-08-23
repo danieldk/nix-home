22a1a6bc3b8f56e6ce9be3d7cdb0c7634d926d0b
Daniël de Kok <me@danieldk.eu>
2018-08-23 22:33:47 +0200

    Install emacs-mac, make aliases work.

--- cfg/emacs.nix
+++ cfg/emacs.nix
@@ -1,44 +1,48 @@
 { pkgs, ... }:
 
-{
-  programs.emacs = {
-    enable = true;
-    extraPackages = epkgs: [
-      epkgs.company
-      epkgs.counsel
-      epkgs.diminish
-      epkgs.evil
-      (epkgs.evil-magit.overrideAttrs (attrs: {
-        nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ [ pkgs.git ];
-      }))
-      epkgs.evil-mu4e
-      epkgs.flycheck
-      epkgs.general
-      epkgs.ivy
-      epkgs.leuven-theme
-      epkgs.nlinum-relative
-      epkgs.magit
-      epkgs.markdown-mode
-      epkgs.nix-mode
-      epkgs.projectile
-      epkgs.swiper
-      epkgs.use-package
-      epkgs.which-key
+let
+  package = if pkgs.stdenv.isDarwin then pkgs.emacsMacport else pkgs.emacs;
+  emacsPackages = pkgs.emacsPackagesNgGen package;
+  emacsWithPackages = emacsPackages.emacsWithPackages (epkgs: [
+    epkgs.company
+    epkgs.counsel
+    epkgs.diminish
+    epkgs.evil
+    (epkgs.evil-magit.overrideAttrs (attrs: {
+      nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ [ pkgs.git ];
+    }))
+    epkgs.evil-mu4e
+    epkgs.flycheck
+    epkgs.general
+    epkgs.ivy
+    epkgs.leuven-theme
+    epkgs.nlinum-relative
+    epkgs.magit
+    epkgs.markdown-mode
+    epkgs.nix-mode
+    epkgs.projectile
+    epkgs.swiper
+    epkgs.use-package
+    epkgs.which-key
 
-      # org-mode
-      epkgs.org
-      epkgs.org-bullets
-      epkgs.org-evil
+    # org-mode
+    epkgs.org
+    epkgs.org-bullets
+    epkgs.org-evil
 
-      # Rust
-      epkgs.cargo
-      epkgs.flycheck-rust
-      epkgs.racer
-      epkgs.rust-mode
-    ];
-  };
+    # Rust
+    epkgs.cargo
+    epkgs.flycheck-rust
+    epkgs.racer
+    epkgs.rust-mode
+  ]);
 
- 
+  emacsBinary = if pkgs.stdenv.isDarwin then
+    "${emacsWithPackages}/Applications/Emacs.app/Contents/MacOS/Emacs"
+  else
+    "${emacsWithPackages}/bin/emacs";
+in {
+  home.packages = [ emacsWithPackages ];
 
   home.file =
     let
@@ -57,7 +61,8 @@
   };
 
   programs.zsh.shellAliases = {
-    mue = "emacs -q --load ~/.emacs-mu4e";
-    org = "emacs -q --load ~/.emacs-org";
+    emacs = "${emacsBinary} -q --load ~/.emacs";
+    mue = "${emacsBinary} -q --load ~/.emacs-mu4e";
+    org = "${emacsBinary} -q --load ~/.emacs-org";
   };
 }
