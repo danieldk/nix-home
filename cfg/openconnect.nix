49f5407f29aa997c9abf607d198add3fed806f30
Daniël de Kok <me@danieldk.eu>
2018-09-01 20:46:41 +0200

    openconnect: fix unitue route.
    The route that the Cisco VPN gives to openconnect is a small subnet
    which does not include hosts that I need to access.

--- cfg/openconnect.nix
+++ cfg/openconnect.nix
@@ -5,7 +5,21 @@
     openconnect
   ];
 
-  programs.zsh.shellAliases = {
-    vpn = "pass Work/UniTuebingen  | sudo openconnect --passwd-on-stdin -u nnsdd01 --no-dtls ras.uni-tuebingen.de";
+  programs.zsh.shellAliases = let
+    vpncScript = with pkgs; pkgs.writeScript "uniTueVpnc" ''
+      # Cisco-provided route only covers a subset of the university
+      # network. Fixup.
+      export INTERNAL_IP4_NETADDR="134.2.0.0"
+      export INTERNAL_IP4_NETMASK="255.255.0.0"
+      export INTERNAL_IP4_NETMASKLEN="16"
+
+      exec ${vpnc}/etc/vpnc/vpnc-script
+    '';
+  in {
+  vpn = builtins.replaceStrings ["\n"] [" "] ''
+    pass Work/UniTuebingen |
+    sudo openconnect --passwd-on-stdin -u nnsdd01 --no-dtls
+      --script ${vpncScript} ras.uni-tuebingen.de
+    '';
   };
 }
