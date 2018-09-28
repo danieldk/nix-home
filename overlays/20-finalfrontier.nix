53cbeaec02334cc940a2892da8445ddb3f042ba4
Daniël de Kok <me@danieldk.eu>
2018-09-28 12:26:26 +0200

    Update finalfrontier.

--- overlays/20-finalfrontier.nix
+++ overlays/20-finalfrontier.nix
@@ -1,19 +1,29 @@
 self: super: {
   finalfrontier = with super; rustPlatform.buildRustPackage rec {
   name = "finalfrontier-${version}";
-  version = "0.0.1";
+  version = "0.2.0-dev";
 
-  src = fetchGit {
-    url = "https://git.danieldk.eu/finalfrontier";
-    ref = "docs";
-    rev = "16c41bc5edfc11081a1fe6e961b021e428020b52";
+  src = fetchFromGitHub {
+    owner = "danieldk";
+    repo = "finalfrontier";
+    rev = "cf9d215f505a760397f4179a997994669864c640";
+    sha256 = "1rgl83ia26ll5g9ih2hh9qjh5qc7fsgd3xhm8gi2yjk9gnb3vc29";
+    #ref = "tags/v0.1.0";
   };
 
   cargoSha256 = "1wpn8x5sdig56h2hia3ic0izwya1yvz5mq89nqjx6vibnj9q4qg1";
 
+  nativeBuildInputs = [ gnumake pandoc ];
+
+  postBuild = ''
+    ( cd man ; make )
+  '';
+
   preFixup = ''
     mkdir -p "$out/man/man1"
     cp man/*.1 "$out/man/man1/"
+    mkdir -p "$out/man/man5"
+    cp man/*.5 "$out/man/man5/"
   '';
 
   meta = with stdenv.lib; {
