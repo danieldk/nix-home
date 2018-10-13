8dbe586f37bc900be3aba3e75600f2ab003aeb6f
Daniël de Kok <me@danieldk.eu>
2018-10-13 10:58:53 +0200

    Use home-manager to manage mbsync and msmtp.

--- cfg/mail.nix
+++ cfg/mail.nix
@@ -1,14 +1,59 @@
 { pkgs, ... }:
 
 {
+  accounts.email.accounts.fastmail = {
+    primary = true;
+
+    address = "me@danieldk.eu";
+    passwordCommand = "${pkgs.pass}/bin/pass Mail/magnolia";
+    realName = "Daniël de Kok";
+    userName = "ddk@fastmail.fm";
+
+    imap = {
+      host = "imap.fastmail.com";
+      port = 993;
+      tls.enable = true;
+    };
+
+    smtp = {
+      host = "smtp.fastmail.com";
+      port = 465;
+      tls.enable = true;
+    };
+
+    mbsync = {
+      enable = true;
+      create = "maildir";
+      expunge = "both";
+      flatten = ".";
+      patterns = [ "*" "!.*" ];
+
+      extraConfig.local = {
+        Subfolders = "Verbatim";
+      };
+    };
+
+    msmtp = {
+      enable = true;
+    };
+
+    notmuch.enable = true;
+  };
+
   home.packages = with pkgs; [
-    isync
-    msmtp
     mu
   ];
 
   home.file = {
-    ".mbsyncrc".source = ./mail/mbsyncrc;
-    ".msmtprc".source = ./mail/msmtprc;
+  };
+
+  programs.mbsync.enable = true;
+  programs.notmuch = {
+    enable = true;
+    extraConfig = {
+      maildir = {
+        synchronize_flags = "true";
+      };
+    };
   };
 }
