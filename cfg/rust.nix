802e16a757261e1863957407a588889a7d4d6578
Daniël de Kok <me@danieldk.eu>
2018-10-22 20:20:10 +0200

    Don't install rustc in path.
    Playing with just using direnv to bring Rust into the path. Makes it
    easier to switch between versions in a nixy way.

--- cfg/rust.nix
+++ cfg/rust.nix
@@ -2,9 +2,10 @@
 
 {
   home.packages = with pkgs; [
-    latest.rustChannels.stable.rust
-    danieldk.rustracer
+    #latest.rustChannels.stable.rust
+    #latest.rustChannels.nightly.rust
+    rustracer
   ];
 
-  programs.zsh.initExtra = "export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src";
+  programs.zsh.initExtra = "export RUST_SRC_PATH=$(${pkgs.latest.rustChannels.stable.rust}/bin/rustc --print sysroot)/lib/rustlib/src/rust/src";
 }
