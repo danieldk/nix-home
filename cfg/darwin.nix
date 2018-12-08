1409b2272e7eb522d8bd7f6dfded181a49b9d9a8
Daniël de Kok <me@danieldk.eu>
2018-12-08 14:57:26 +0100

    Install Dash/iTerm.

--- cfg/darwin.nix
+++ cfg/darwin.nix
@@ -1,14 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [];
-
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+  environment.systemPackages = with pkgs; [
+    Dash
+    iTerm2
+  ];
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
@@ -16,7 +12,6 @@
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.bash.enable = true;
   programs.zsh.enable = true;
-  # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
