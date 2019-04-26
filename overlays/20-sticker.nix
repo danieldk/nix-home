00e49395b63a923043214dafe94a3a35593c8b3a
Daniël de Kok <me@danieldk.eu>
2019-04-26 17:21:25 +0200

    Update sticker to 0.2.0

--- overlays/20-sticker.nix
+++ overlays/20-sticker.nix
@@ -1,25 +1,50 @@
 self: super: {
+  libtensorflow_113 = with super; let
+    system =
+      if stdenv.isx86_64 then
+        if stdenv.isLinux then "linux-x86_64"
+        else if stdenv.isDarwin then "darwin-x86_64"
+        else unavailable
+      else unavailable;
+    unavailable = throw "libtensorflow is not available for this platform!";
+  in libtensorflow.overrideAttrs (oldAttrs: rec {
+    pname = "libtensorflow";
+    name = "${pname}-${version}";
+    version = "1.13.1";
+
+    src = fetchurl {
+      url = "https://storage.googleapis.com/tensorflow/${pname}/${pname}-cpu-${system}-${version}.tar.gz";
+      sha256 =
+        if system == "linux-x86_64" then
+            "0cfnpqz1s6ilajg0nfirwbp18f3hls2q8bhn6z032jxn90llhk4k"
+        else if system == "darwin-x86_64" then
+            "0mv0xrnkb871l6j6gpdq84aq5987sabszggl1jyws4hvacipidmq"
+        else unavailable;
+    };
+  });
+
   sticker = with super; rustPlatform.buildRustPackage rec {
-  name = "sticker-${version}";
-  version = "0.1.0";
+    name = "sticker-${version}";
+    version = "0.2.0";
 
-  src = fetchgit {
-    url = "https://git.sr.ht/~danieldk/sticker";
-    rev = "90d00fff4617294825474b1acead803446d243e2";
-    sha256 = "0350rnlrwb3yl5wjp4lqqsnd2h2bsiflphahml5gg9mi9n6s692r";
+    src = fetchFromGitHub {
+        owner = "danieldk";
+        repo = "sticker";
+        rev = "${version}";
+        sha256 = "1b4gsxza6kqaialadh3f2snmgx39db7p315ymk1gad1m10c99gc7";
+    };
+
+    cargoSha256 = "0b5x71hrs7qb1sbp1ijb75v2nnrmay4c9a41g0lzwz9qk4q0s51r";
+
+    nativeBuildInputs = [ pkg-config ];
+
+    buildInputs = with super; [ openssl self.libtensorflow_113 ] ++
+      stdenv.lib.optional stdenv.isDarwin darwin.Security;
+
+    meta = with stdenv.lib; {
+        description = "Neural sequence labeler";
+        license = licenses.asl20;
+        platforms = platforms.all;
+    };
   };
-
-  cargoSha256 = "17pvy8006lkbfpg2cl0fjz0ilq9a309lwni6xl9wz7gydjn8agsy";
-
-  buildInputs = with super ; [ libtensorflow ] ++
-    stdenv.lib.optionals stdenv.isDarwin [curl darwin.Security];
-
-  meta = with stdenv.lib; {
-    description = "Neural sequence tagger";
-    license = licenses.asl20;
-    platforms = platforms.all;
-  };
-
-};
-
 }
