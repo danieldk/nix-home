d84f004dabf8421dfe458aa2608910c57a009734
Daniël de Kok <me@danieldk.eu>
2019-05-17 20:42:24 +0200

    Remove danieldk-overlay

--- overlays/00-danieldk-overlay.nix
+++
@@ -1,8 +0,0 @@
-self: super: {
-  danieldk = import (self.fetchFromGitHub {
-    owner = "danieldk";
-    repo = "nixpkgs";
-    rev = "known-good";
-    sha256 = "0slcnhdh081cqflc3g17sfl0q628yf2dfpm8vxs672x9nvj0pyq5";
-  }) {};
-}
