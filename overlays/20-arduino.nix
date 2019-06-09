94ab66302025e1fbbf72047e10b2d737993711ad
Daniël de Kok <me@danieldk.eu>
2019-06-09 13:16:38 +0200

    Add Arduino overlay
    This adds 'arduino-fhs' which runs the Arduino IDE in a FHS environment
    to support 3rd party SDKs (e.g. esp32).

---
+++ overlays/20-arduino.nix
@@ -0,0 +1,17 @@
+self: super: {
+
+arduino-fhs =
+  super.buildFHSUserEnv {
+    name = "arduino-fhs";
+
+    targetPkgs = pkgs: with pkgs; [
+      arduino
+      zlib
+      (python3.withPackages(ps: [
+        ps.pyserial
+      ]))
+    ];
+
+    multiPkgs = null;
+  };
+}
