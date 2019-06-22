a3b198ef4be2200c5c52d6d9de43eda485c90c53
Daniël de Kok <me@danieldk.eu>
2019-06-22 10:57:13 +0200

    Remove finalfusion package set
    (All the packages are now absorbed by my package set.)

--- config.nix
+++ config.nix
@@ -5,9 +5,5 @@
     danieldk = import ./danieldk-nix-packages/default.nix {
       inherit pkgs;
     };
-
-    finalfusion = import ./finalfusion-overlay/default.nix {
-      inherit pkgs;
-    };
   };
 }
