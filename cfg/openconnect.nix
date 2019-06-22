6c70f64b2404155978c24d2b1f6f1f48aada4322
Daniël de Kok <me@danieldk.eu>
2019-06-22 10:50:53 +0200

    Turn 'vpn' into a script, add XDG desktop item

--- cfg/openconnect.nix
+++ cfg/openconnect.nix
@@ -1,25 +1,30 @@
 { pkgs, ... }:
 
-{
-  home.packages = with pkgs; [
-    openconnect
-  ];
+let
+  vpncScript = pkgs.writeScript "uniTueVpnc" ''
+    # Cisco-provided route only covers a subset of the university
+    # network. Fixup.
+    export INTERNAL_IP4_NETADDR="134.2.0.0"
+    export INTERNAL_IP4_NETMASK="255.255.0.0"
+    export INTERNAL_IP4_NETMASKLEN="16"
 
-  programs.zsh.shellAliases = let
-    vpncScript = with pkgs; pkgs.writeScript "uniTueVpnc" ''
-      # Cisco-provided route only covers a subset of the university
-      # network. Fixup.
-      export INTERNAL_IP4_NETADDR="134.2.0.0"
-      export INTERNAL_IP4_NETMASK="255.255.0.0"
-      export INTERNAL_IP4_NETMASKLEN="16"
-
-      exec ${vpnc}/etc/vpnc/vpnc-script
-    '';
-  in {
-  vpn = builtins.replaceStrings ["\n"] [" "] ''
-    pass Work/UniTuebingen |
-    sudo openconnect --passwd-on-stdin -u nnsdd01 --no-dtls
-      --script ${vpncScript} ras.uni-tuebingen.de
+    exec ${pkgs.vpnc}/etc/vpnc/vpnc-script
+  '';
+  vpnScript = pkgs.writeScriptBin "vpn" ''
+    #! ${pkgs.bash}/bin/sh
+    pass Work/UniTuebingen | \
+    sudo ${pkgs.openconnect}/bin/openconnect --passwd-on-stdin -u nnsdd01 --no-dtls \
+    --script ${vpncScript} ras.uni-tuebingen.de
+  '';
+in {
+  home.packages = [ vpnScript ];
+  home.file = {
+    ".local/share/applications/vpn.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=vpn
+      Exec=${vpnScript}/bin/vpn
+      Terminal=true
     '';
   };
 }
