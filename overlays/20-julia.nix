8cfeb0a8c128867a49a5e73e4f0fcc4a841b0d4f
Daniël de Kok <me@danieldk.eu>
2019-09-13 19:38:34 +0200

    Add Julia overlay

---
+++ overlays/20-julia.nix
@@ -0,0 +1,60 @@
+self: super: {
+
+julia-fhs =
+let
+  ldconfigWrapper = with super; stdenv.mkDerivation {
+    name = "ldconfig-env";
+
+    nativeBuildInputs = [ makeWrapper ];
+
+    phases = [ "installPhase" "fixupPhase" ];
+
+    installPhase = ''
+      makeWrapper ${glibc.bin}/bin/ldconfig $out/sbin/ldconfig \
+        --add-flags "-C /usr/ld.so.cache"
+    '';
+  };
+in
+  super.buildFHSUserEnv {
+    name = "julia-fhs";
+  
+    targetPkgs = pkgs: with pkgs; [
+      ldconfigWrapper
+  
+      julia_11
+  
+      autoconf
+      binutils
+      coreutils
+      gnumake
+      m4
+      pkgconfig
+      stdenv.cc
+      utillinux
+  
+      curl
+      git
+  
+      cairo
+      fontconfig
+      freetype
+      gettext
+      glib.out
+      libintl
+      libpng
+      libffi
+      librsvg
+      pango.out
+      pixman
+      zlib
+    ];
+  
+    extraBuildCommands = with super; ''
+      # Cannot write to /etc?
+      echo "$out/lib" > $out/usr/ld.so.conf
+      ldconfig -f $out/usr/ld.so.conf -C $out/usr/ld.so.cache -v
+    '';
+  
+    runScript = "julia";
+  };
+}
