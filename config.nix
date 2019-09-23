ffa6b339671b8dda4a60b639673fc1aad34bf845
Daniël de Kok <me@danieldk.eu>
2019-09-23 10:16:18 +0200

    Add the sticker package set

--- config.nix
+++ config.nix
@@ -5,5 +5,8 @@
     danieldk = pkgs.recurseIntoAttrs (import ./danieldk-nix-packages/default.nix {
       inherit pkgs;
     });
+    sticker = pkgs.recurseIntoAttrs (import ./sticker/default.nix {
+      inherit pkgs;
+    });
   };
 }
