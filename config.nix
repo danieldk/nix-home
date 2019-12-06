88f073c82eeac53c182146e2b350d5f2bd480411
Daniël de Kok <me@danieldk.eu>
2019-12-06 09:01:20 +0100

    Add finalfusion package set

--- config.nix
+++ config.nix
@@ -5,6 +5,11 @@
     danieldk = pkgs.recurseIntoAttrs (import ./danieldk-nix-packages/default.nix {
       inherit pkgs;
     });
+
+    finalfusion = pkgs.recurseIntoAttrs (import ./finalfusion/default.nix {
+      inherit pkgs;
+    });
+
     sticker = pkgs.recurseIntoAttrs (import ./sticker/default.nix {
       inherit pkgs;
     });
