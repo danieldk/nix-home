94db437ee84f3eb3c975986780eb9e33a2218eb8
Daniël de Kok <me@danieldk.eu>
2019-12-14 11:39:04 +0100

    Add ROCm package set
    This uses my fork of nixos-rocm until nixos-rocm/nixos-rocm#25 is
    (hopefully) accepted.

--- config.nix
+++ config.nix
@@ -1,5 +1,6 @@
 let
   sources = import ./nix/sources.nix;
+  nixpkgs-unstable = import sources.nixpkgs-unstable {};
 in {
   allowUnfree = true;
 
@@ -17,6 +18,10 @@ in {
     };
 
     mozilla = pkgs.callPackage "${sources.mozilla}/package-set.nix" {};
+
+    rocm = let
+      self = (import sources.rocm) (nixpkgs-unstable // self) nixpkgs-unstable;
+    in self;
 
     sticker = pkgs.recurseIntoAttrs (import ./sticker/default.nix {
       inherit pkgs;
