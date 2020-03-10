8564a1fcd266c87524a2b2f0eb500e73da05df91
Daniël de Kok <me@danieldk.eu>
2020-03-10 15:22:17 +0100

    zsh: add fzf-tab

--- cfg/zsh.nix
+++ cfg/zsh.nix
@@ -55,6 +55,16 @@
           sha256 = "1h0yk0sbv9d571sfkg97wi5q06cpxnhnvh745dlpazpgqi1vb1a8";
         };
       }
+      {
+        name = "fzf-tab";
+        file = "fzf-tab.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "Aloxaf";
+          repo = "fzf-tab";
+          rev = "17ff089938a8f693689d67814af3212fc4053da1";
+          sha256 = "1cizy2nlmvrblirxkl7036fnp1q55s11v5pxm7x64jx1591iy67s";
+        };
+      }
     ];
   };
 }
