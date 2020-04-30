104c82911c47468dcd0d27a73e783ecc5548f75d
Daniël de Kok <me@danieldk.eu>
2020-04-30 19:30:32 +0200

    emacs: cleanup configuration

--- cfg/emacs.nix
+++ cfg/emacs.nix
@@ -1,69 +1,56 @@
 { pkgs, ... }:
 
-let
-  package = if pkgs.stdenv.isDarwin then pkgs.emacsMacport else pkgs.emacs;
-  emacsPackages = pkgs.emacsPackagesNgGen package;
-  emacsWithPackages = emacsPackages.emacsWithPackages (epkgs: with epkgs; [
-    auctex
-    avy
-    company
-    company-math
-    counsel
-    cython-mode
-    diminish
-    direnv
-    evil
-    evil-magit
-    evil-mu4e
-    flycheck
-    forge
-    general
-    go-mode
-    ivy
-    ivy-bibtex
-    leuven-theme
-    lsp-ivy
-    lsp-mode
-    lsp-ui
-    pkgs.mu
-    nlinum-relative
-    magit
-    markdown-mode
-    nix-mode
-    nix-update
-    projectile
-    swiper
-    use-package
-    which-key
-    zenburn-theme
+{
+  home.packages = [ pkgs.cargo pkgs.rust-analyzer pkgs.rustfmt ];
 
-    # org-mode
-    org
-    org-bullets
-    evil-org
+  programs.emacs = {
+    enable = true;
 
-    # Rust
-    cargo
+    extraPackages = epkgs: with epkgs; [
+      auctex
+      avy
+      company
+      company-math
+      counsel
+      cython-mode
+      diminish
+      direnv
+      evil
+      evil-magit
+      flycheck
+      forge
+      general
+      go-mode
+      ivy
+      ivy-bibtex
+      leuven-theme
+      lsp-ivy
+      lsp-mode
+      lsp-ui
+      pkgs.mu
+      nlinum-relative
+      magit
+      markdown-mode
+      nix-mode
+      nix-update
+      projectile
+      swiper
+      use-package
+      which-key
+      zenburn-theme
 
-    # Protocol Buffers
-    protobuf-mode
-  ]);
+      # org-mode
+      org
+      org-bullets
+      evil-org
 
-  emacsBinary = if pkgs.stdenv.isDarwin then
-    "${emacsWithPackages}/Applications/Emacs.app/Contents/MacOS/Emacs"
-  else
-    "${emacsWithPackages}/bin/emacs";
+      # Rust
+      cargo
 
-  orgMacs = pkgs.writeScriptBin "org" ''
-    #!/bin/sh
-    ${emacsBinary} -q --load ~/.emacs-org $@
-  '';
-  muMacs = pkgs.writeScriptBin "mue" ''
-    #!/bin/sh
-    ${emacsBinary} -q --load ~/.emacs-mu4e $@
-  '';
-in {
-  home.packages = [ pkgs.cargo pkgs.rust-analyzer pkgs.rustfmt emacsWithPackages orgMacs muMacs ];
+      # Protocol Buffers
+      protobuf-mode
+    ];
+  };
 
   home.file =
     let
@@ -78,8 +65,4 @@ in {
     ".emacs-org".text = emacsFont + builtins.readFile ./emacs/base +
       builtins.readFile ./emacs/org;
   };
-
- programs.zsh.shellAliases = {
-   emacs = "${emacsBinary} -q --load ~/.emacs";
- };
 }
