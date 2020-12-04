24cc53c4e7f7ba22d64ee6cff8b93681b339014d
Daniël de Kok <me@danieldk.eu>
2020-12-04 07:49:24 +0100

    ssh: remove Uni Tübingen hosts

--- home/cfg/ssh.nix
+++ home/cfg/ssh.nix
@@ -9,35 +9,6 @@
     '';
 
     matchBlocks = {
-      "aion" = {
-        hostname = "aion.sfs.uni-tuebingen.de";
-        user = "daniel";
-      };
-
-      "hopper" = {
-        hostname = "hopper.sfs.uni-tuebingen.de";
-        user = "daniel";
-      };
-
-      "shaw" = {
-        hostname = "shaw.sfs.uni-tuebingen.de";
-        user = "daniel";
-      };
-
-      "tesniere" = {
-        hostname = "tesniere.sfs.uni-tuebingen.de";
-        user = "ddekok";
-      };
-
-      "turing" = {
-        hostname = "turing.sfs.uni-tuebingen.de";
-        user = "ddekok";
-      };
-
-      "turing-sfb" = {
-        hostname = "turing-sfb.sfs.uni-tuebingen.de";
-        user = "daniel";
-      };
     };
   };
 }
