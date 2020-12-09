418b5956a62d79c0bc6adaad4f99a2c4151bede3
Daniël de Kok <me@danieldk.eu>
2020-12-09 15:56:28 +0100

    Use the pinned nixpkgs version for nix commands

--- flake.nix
+++ flake.nix
@@ -26,29 +26,39 @@
   outputs = { self, alpinocorpus, conllu-utils, crate2nix, dwarffs, home-manager, nixpkgs }: {
     overlays.scripts = import overlays/20-scripts.nix;
 
-    nixosConfigurations.mindbender = let
-      system = "x86_64-linux";
-    in nixpkgs.lib.nixosSystem {
-      inherit system;
+    nixosConfigurations = let
+      commonModule = {
+        # Use the pinned nixpkgs version for flake commands.
+        nix.registry.nixpkgs.flake = nixpkgs;
 
-      modules = [
-        dwarffs.nixosModules.dwarffs
-        nixos/machines/mindbender.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.daniel = import home/machines/mindbender.nix;
-          nixpkgs.overlays = [
-            (final: prev: {
-              alpinocorpus = alpinocorpus.defaultPackage.${system};
-              conllu-utils = conllu-utils.defaultPackage.${system};
-              crate2nix = final.callPackage crate2nix {};
-            })
-            self.overlays.scripts
-          ];
-        }
-      ];
+        nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+      };
+    in {
+      mindbender = let
+        system = "x86_64-linux";
+      in nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          commonModule
+          dwarffs.nixosModules.dwarffs
+          nixos/machines/mindbender.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.daniel = import home/machines/mindbender.nix;
+            nixpkgs.overlays = [
+              (final: prev: {
+                alpinocorpus = alpinocorpus.defaultPackage.${system};
+                conllu-utils = conllu-utils.defaultPackage.${system};
+                crate2nix = final.callPackage crate2nix {};
+              })
+              self.overlays.scripts
+            ];
+          }
+        ];
+      };
     };
   };
 }
