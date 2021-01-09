93b11b64a5e18e2f86bc85454f958996dcc492ac
Daniël de Kok <me@danieldk.eu>
2021-01-09 12:39:43 +0100

    Remove crate2nix as a flake input, it is now in nixpkgs

--- flake.nix
+++ flake.nix
@@ -8,10 +8,6 @@
       url = "github:danieldk/conllu-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crate2nix = {
-      url = "github:kolloch/crate2nix/0.8.0";
-      flake = false;
-    };
     dwarffs = {
       url = "github:edolstra/dwarffs/83c13981993fa54c4cac230f2eec7241ab8fd0a9";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,7 +19,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   };
 
-  outputs = { self, alpinocorpus, conllu-utils, crate2nix, dwarffs, home-manager, nixpkgs }: {
+  outputs = { self, alpinocorpus, conllu-utils, dwarffs, home-manager, nixpkgs }: {
     overlays.scripts = import overlays/20-scripts.nix;
 
     nixosConfigurations = let
@@ -51,7 +47,6 @@
               (final: prev: {
                 alpinocorpus = alpinocorpus.defaultPackage.${system};
                 conllu-utils = conllu-utils.defaultPackage.${system};
-                crate2nix = final.callPackage crate2nix {};
               })
               self.overlays.scripts
             ];
