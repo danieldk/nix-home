5e0a440dd4b63574c1a1f80038296087dcc95bc3
Daniël de Kok <me@danieldk.eu>
2024-12-10 09:58:36 +0000

    tgi-dev: make SSH agent work in older zellij sessions

--- home/machines/daniel-tgi-dev.nix
+++ home/machines/daniel-tgi-dev.nix
@@ -21,4 +21,19 @@
   programs.git.extraConfig = {
     credential.helper = "store";
   };
+
+  programs.zsh.initExtra = ''
+    # https://github.com/zellij-org/zellij/issues/1637#issuecomment-2325124511
+    if [ -z "''${ZELLIJ}" ] && [ "''${TERM}" != 'dumb' ] && ( echo "$-" | grep "i" >/dev/null ) && command -v zellij >/dev/null; then
+      if [ -S ''${SSH_AUTH_SOCK} ] && [ -w "/run/user/$(id -u)/" ] && ! [ -S "/run/user/$( id -u )/ssh_auth.sock" ]; then
+          rm -f /run/user/$(id -u)/ssh_auth.sock
+          ln -s "''${SSH_AUTH_SOCK}" "/run/user/$(id -u)/ssh_auth.sock"
+      fi
+    #exec zellij attach -c main
+    fi
+
+    if [ -S "/run/user/$(id -u)/ssh_auth.sock" ]; then
+        export SSH_AUTH_SOCK="/run/user/$(id -u)/ssh_auth.sock"
+    fi
+  '';
 }
