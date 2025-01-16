7a6e0e816d3346be3634bc64bb7529042431c537
Daniël de Kok <me@danieldk.eu>
2025-01-16 15:50:31 +0000

    builder: repl-flake feature does not exist anymore

--- nixos/machines/builder.nix
+++ nixos/machines/builder.nix
@@ -31,7 +31,7 @@
     settings.sandbox = true;
     settings.trusted-users = [ "daniel" ];
     extraOptions = ''
-      experimental-features = nix-command flakes repl-flake
+      experimental-features = nix-command flakes
     '';
   };
 
