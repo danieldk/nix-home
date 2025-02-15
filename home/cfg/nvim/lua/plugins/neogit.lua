604e6571e5405dc8f2c8fa884e2be38ee6575890
Daniël de Kok <me@danieldk.eu>
2025-02-15 10:10:17 +0100

    neogit: disable diffview and replace telescope by fzf-lua

--- home/cfg/nvim/lua/plugins/neogit.lua
+++ home/cfg/nvim/lua/plugins/neogit.lua
@@ -4,8 +4,7 @@ return {
     cmd = "Neogit",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",
     },
     config = true,
     keys = { { "<leader>gn", "<cmd>Neogit<CR>", desc = "Open Neogit" } },
