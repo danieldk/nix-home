a6f8e4bf2bd64a7023cdecf6c478fb59697055ae
Daniël de Kok <me@danieldk.eu>
2025-02-25 17:57:19 +0100

    lerisque: hostname

--- nixos/machines/lerisque.nix
+++ nixos/machines/lerisque.nix
@@ -17,12 +17,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "lerisque";
 
   # Enable networking
   networking.networkmanager.enable = true;
