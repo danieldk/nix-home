f4cb9571a199d6b5aa68cfe35f4af210fe666784
Daniël de Kok <me@danieldk.eu>
2025-03-29 19:13:48 +0100

    Add kopia-ui desktop item/icons

--- overlays/kopia-ui.nix
+++ overlays/kopia-ui.nix
@@ -9,9 +9,19 @@ self: super: {
         url = "https://github.com/kopia/kopia/releases/download/v${version}/KopiaUI-${version}.AppImage";
         hash = "sha256-ja1a5VoWCqnYtUZUzMSk5pMweguOnXPFvvFRKuMKCKw=";
       };
+      appimageContents = appimageTools.extractType2 { inherit pname version src; };
     in
     appimageTools.wrapType2 {
       inherit pname version src;
+
+      extraInstallCommands = ''
+        install -Dm444 \
+          ${appimageContents}/kopia-ui.desktop \
+          -t $out/share/applications
+        substituteInPlace $out/share/applications/kopia-ui.desktop \
+          --replace-fail "AppRun --no-sandbox %U" "kopia-ui"
+        cp -r ${appimageContents}/usr/share/icons $out/share
+      '';
     };
 
 }
