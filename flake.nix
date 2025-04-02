3f8396c83edfcd49dbd770e1cb2520eecdc3ab0d
Daniël de Kok <me@danieldk.eu>
2025-04-02 09:25:47 +0200

    Enable asdbctl overlay

--- flake.nix
+++ flake.nix
@@ -73,6 +73,7 @@
                 pkgs = self;
               };
             })
+            (import overlays/asdbctl.nix)
             (import overlays/fonts.nix)
             (import overlays/sandboxing.nix)
             (import overlays/fixup.nix)
