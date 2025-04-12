bc8516355af75801ddc7f94fd8786e7e188d6497
Daniël de Kok <me@danieldk.eu>
2025-04-12 08:04:18 +0200

    desktop: don't forget to enable the smile shortcut

--- home/cfg/desktop.nix
+++ home/cfg/desktop.nix
@@ -47,6 +47,7 @@
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
         ];
       };
 
