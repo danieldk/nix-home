7582bcaa7723c0df884b93491d8b18184ea144e8
Daniël de Kok <me@danieldk.eu>
2025-07-15 08:49:07 +0200

    lerisque: better power saving

--- nixos/machines/lerisque.nix
+++ nixos/machines/lerisque.nix
@@ -118,6 +118,11 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
 
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
