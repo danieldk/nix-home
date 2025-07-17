482965878a4c95505eb7f7c769519b81aca860b9
Daniël de Kok <me@danieldk.eu>
2025-07-17 08:04:07 +0200

    nonagon: predictable SSH socket path
    Fixes multi-SSH session zellij use.

--- home/machines/nonagon.nix
+++ home/machines/nonagon.nix
@@ -20,5 +20,19 @@
 
   #services.vscode-server.enable = true;
 
+  programs.zsh.initContent = ''
+    # https://github.com/zellij-org/zellij/issues/1637#issuecomment-2325124511
+    if [ -z "''${ZELLIJ}" ] && [ "''${TERM}" != 'dumb' ] && ( echo "$-" | grep "i" >/dev/null ) && command -v zellij >/dev/null; then
+      if [ -S ''${SSH_AUTH_SOCK} ] && [ -w "/run/user/$(id -u)/" ] && ! [ -S "/run/user/$( id -u )/ssh_auth.sock" ]; then
+          rm -f /run/user/$(id -u)/ssh_auth.sock
+          ln -s "''${SSH_AUTH_SOCK}" "/run/user/$(id -u)/ssh_auth.sock"
+      fi
+    fi
+
+    if [ -S "/run/user/$(id -u)/ssh_auth.sock" ]; then
+        export SSH_AUTH_SOCK="/run/user/$(id -u)/ssh_auth.sock"
+    fi
+  '';
+
   home.stateVersion = "25.11";
 }
