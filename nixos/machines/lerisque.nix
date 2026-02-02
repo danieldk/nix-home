c0f5a3cbb31e1c51dfbed29beac705deed593b2a
Daniël de Kok <me@danieldk.eu>
2026-02-02 09:55:20 +0100

    lerisque: fresh install, fresh state version

--- nixos/machines/lerisque.nix
+++ nixos/machines/lerisque.nix
@@ -9,7 +9,7 @@
   imports = [
     ./lerisque-hwconf.nix
     ../cfg/base-nixos.nix
-    ../cfg/desktop-gnome3.nix
+    ../cfg/desktop-plasma5.nix
     ../cfg/clamav.nix
   ];
 
@@ -148,6 +148,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 
 }
